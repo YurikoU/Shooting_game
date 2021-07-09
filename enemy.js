c1bdf20ca97e161ad7b6e2bb81b1b71c082e7cc6
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-07-08 17:20:06 -0700

    Add files via upload

--- enemy.js
+++ enemy.js
@@ -17,24 +17,34 @@ class Enemy extends CharacterBase {
 
         if ( !this.flag ) {
             //The enemy will approach to jiki, if the enemies are far from jiki
-            if ( this.x < jiki.x && this.vectorX < 120 ) {
+            if ( this.x < jiki.x  &&  this.vectorX < 120 ) {
                 this.vectorX += 4; 
-            } else if ( jiki.x < this.x && -120 < this.vectorX ) {
+            } else if ( jiki.x < this.x  &&  -120 < this.vectorX ) {
                 this.vectorX -= 4;
             }
         } else {
             //The enemies will escape, if the enemies are near jiki
-            if ( jiki.x < this.x  && this.vectorX < 400 ) {
+            if ( jiki.x < this.x  &&  this.vectorX < 400 ) {
                 this.vectorX += 30; 
             } else if ( this.x < jiki.x && -400 < this.vectorX ) {
                 this.vectorX -= 30;
             }
         }
         
-        if ( Math.abs(jiki.y - this.y) < (100<<8) ) {//If the enemies come near to jiki within 100px
+        if ( Math.abs(jiki.y - this.y) < (100<<8)  &&  !this.flag ) {//If the enemies come near to jiki within 100px
             this.flag = true;
 
-            enemyBullet.push( new EnemyBullet(15, this.x, this.y, 0, 1200) );
+            //Math.atan2        : 0 to 2 (radian)
+            //Math.cos, Math.sin: 0 to 1
+            let angleFromEnemyToJiki   = Math.atan2( (jiki.y - this.y), (jiki.x - this.x) );
+            angleFromEnemyToJiki      += rand( -10, 10 ) * Math.PI / 180;
+            let vectorXFromEnemyToJiki = Math.cos( angleFromEnemyToJiki ) * 1000;
+            let vectorYFromEnemyToJiki = Math.sin( angleFromEnemyToJiki ) * 1000;
+
+
+            enemyBullet.push( 
+                new EnemyBullet( 15, this.x, this.y, vectorXFromEnemyToJiki, vectorYFromEnemyToJiki )
+            );
 
             if ( -800 < this.vectorY ) {//The enemies will escape
                 this.vectorY -= 30;
